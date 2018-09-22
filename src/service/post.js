ffbc638d896fdf7257adc3c623c3eff33ddcb8fa
jonathan <jonathan.araya.m@gmail.com>
2018-09-21 21:21:04 -0300

    [service] crud post

---
+++ src/service/post.js
@@ -0,0 +1,44 @@
+const CORE_ENDPOINT = 'http://reduxblog.herokuapp.com/api/posts';
+
+class PostService {
+    async get(id) {
+        const url = `${CORE_ENDPOINT}/${id}`;
+        const response = await fetch(url, {
+            method: 'GET'
+        });
+        if (!response.ok) {
+            return new Error(`Status ${response.status}`);
+        }
+        return await response.json();
+    }
+
+    async post(data) {
+        const url = CORE_ENDPOINT;
+        const response = await fetch(url, {
+            method: 'POST',
+            body: JSON.stringify(data)
+        });
+        return await response.json();
+    }
+
+    async list() {
+        const url = `${CORE_ENDPOINT}`;
+        const response = await fetch(url, {
+            method: 'GET',
+        });
+        if (!response.ok) {
+            return new Error(`Status ${response.status}`);
+        }
+        return await response.json();
+    }
+    async remove(id) {
+        console.log('remmove',id);
+        const url = `${CORE_ENDPOINT}/${id}`;
+        const response = await fetch(url, {
+            method: 'DELETE',
+        });
+        return await response.json();
+    }
+}
+
+export default new PostService();
