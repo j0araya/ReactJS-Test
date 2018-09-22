b044ab6d0e7f50f1230601b3b7351016a7fdf778
jonathan <jonathan.araya.m@gmail.com>
2018-09-21 21:23:25 -0300

    [app] middlewares + redux + provider

--- src/App.js
+++ src/App.js
@@ -1,21 +1,138 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import { connect } from 'react-redux';
+
+import * as actions from './store/post/actions';
+import * as reducers from './store/post/reducer';
+import autoBind from 'react-autobind';
+
+
+
+import {
+  Table,
+  Button,
+  Container,
+  Col,
+  Row,
+  Modal,
+  ModalHeader,
+  ModalBody,
+  ModalFooter
+} from 'reactstrap';
+
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    autoBind(this);
+    this.state = {
+      list: [],
+      modal: false,
+      selectedPost: {}
+    };
+    this.selectedPost = 0;
+  }
+
+  componentDidMount() {
+    this.props.dispatch(actions.list());
+  }
+
+  buttonDelete(id) {
+    console.log('buttom', id);
+    return (
+      <Button color="danger" onClick={() => this.selectItemToDelete(id)}>Delete</Button>
+    )
+  }
+
+  buttonView(id) {
+    return (
+      <Button color="info" onClick={() => this.props.history.push('/post/' + id)}>View</Button>
+    )
+  }
+
+  buttonCreate() {
+    return (
+      <Button color="primary" onClick={() => this.props.history.push('/post/')}>Create</Button>
+    )
+  }
+  selectItemToDelete(id) {
+    this.selectedPost = id;
+    this.toggle();
+  }
+
+  removeItem() {
+    this.props.dispatch(actions.remove(this.selectedPost));
+    this.toggle();
+  }
+
+  toggle() {
+    this.setState({
+      modal: !this.state.modal
+    });
+  }
+
   render() {
+    // const { selectedPost } = this.state;
+    const { posts = [] } = this.props;
+    console.log('data', posts, this.props);
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Container>
+          <Row>
+            <Col className="text-right">
+              {this.buttonCreate()}
+            </Col>
+          </Row>
+          <Row>
+            <Table striped hover responsive>
+              <thead>
+                <tr>
+                  <th>#</th>
+                  <th>Title</th>
+                  <th>Content</th>
+                  <th>Categories</th>
+                  <th>View</th>
+                  <th>Delete</th>
+                </tr>
+              </thead>
+              <tbody>
+                {posts.map((p, i) => (
+                  <tr key={p.id}>
+                    <th scope="row">{i + 1}</th>
+                    <td>{p.title || 'unknown'}</td>
+                    <td>{p.content || 'unknown'}</td>
+                    <td>{p.categories || 'unknown'}</td>
+                    <td>{this.buttonView(p.id)}</td>
+                    <td>{this.buttonDelete(p.id)}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </Table>
+          </Row>
+        </Container>
+        <Modal isOpen={this.state.modal} toggle={this.toggle} className="danger">
+          <ModalHeader toggle={this.toggle}>DELETE</ModalHeader>
+          <ModalBody>
+             Are you sure to eliminate this item?
+          </ModalBody>
+          <ModalFooter>
+            <Button color="danger" onClick={() => this.removeItem()}>Delete</Button>{' '}
+            <Button color="secondary" onClick={this.toggle}>Cancel</Button>
+          </ModalFooter>
+        </Modal>
       </div>
     );
   }
 }
 
-export default App;
+function mapStateToProps(state) {
+  const posts = reducers.list(state);
+  return {
+    posts,
+  };
+}
+
+export default connect(mapStateToProps)(App);
--- src/index.js
+++ src/index.js
@@ -2,7 +2,54 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Post from './containers/post/post';
+import 'bootstrap/dist/css/bootstrap.css';
 import registerServiceWorker from './registerServiceWorker';
+import { createStore, compose, applyMiddleware } from 'redux';
+import { connectRouter, routerMiddleware } from 'connected-react-router';
+import thunk from 'redux-thunk';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { BrowserRouter } from 'react-router-dom';
+
+import { createBrowserHistory } from 'history';
+
+import { Provider } from 'react-redux';
+import * as reducers from './store/reducers';
+
+import { Route, Switch } from 'react-router'; // react-router v4
+
+const history = createBrowserHistory();
+const enhancers = [];
+const middleware = [
+    thunk,
+    routerMiddleware(history)
+];
+
+const composedEnhancers = compose(
+    applyMiddleware(...middleware),
+    ...enhancers
+);
+
+
+const store = createStore(
+    connectRouter(history)(reducers.post),
+    composedEnhancers
+);
+
+
+ReactDOM.render(
+    <Provider store={store}>
+        <BrowserRouter>
+            <div>
+                <Switch>
+                    <Route exact path="/" component={App} />
+                    <Route path="/post/:id" component={Post} />
+                    <Route path="/post/" component={Post} />
+                </Switch>
+            </div>
+        </BrowserRouter>
+    </Provider>
+    , document.getElementById('root')
+);
+
 registerServiceWorker();
